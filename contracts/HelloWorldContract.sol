5fce116761286080d1a3639317a58385ce5f326a
Benni Russell <bennirus@Sarahs-MacBook-Pro.local>
2017-05-19 08:51:22 +0100

    update HelloWorldContract

--- contracts/HelloWorldContract.sol
+++ contracts/HelloWorldContract.sol
@@ -1,14 +1,26 @@
 pragma solidity ^0.4.4;
-
-contract HelloWorldContract {
+contract Ballot {
     
     string word;
+    address issuer;
+    
+    function HelloWorldContract() {
+        issuer = msg.sender;
+    }
+    
+    modifier ifIssuer() {
+        if (issuer != msg.sender) {
+            throw;
+        } else {
+            _;
+        }
+    }
     
     function getWord() constant returns (string){
         return word;
     }
     
-    function setWord(string _word) returns (string){
+    function setWord(string _word) ifIssuer returns (string){
         word = _word;
     }
 }
